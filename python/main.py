decba5ced178543be7bc892df56d6166c0d8651c
Jax Young <jaxvanyang@gmail.com>
2020-12-28 22:44:56 +0800

    make /python/algorithms/ a package

--- python/main.py
+++ python/main.py
@@ -1,5 +1,15 @@
 import sys
 sys.path[0] = 'D:/repos/lang-study/python'
 from file.data import *
+from algorithms.sort import * 
 
-print(get_random_int_list())
+def main():
+    arr = get_random_int_list()
+    arr_sorted = sorted(arr)
+    arr_ = insertion_sort(arr)
+    print(arr_sorted)
+    print(arr_)
+    print(arr_ == arr_sorted)
+
+if __name__ == '__main__':
+    main()
