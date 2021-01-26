1d6d584cde4a4cee95d1365737cb776f2c3b4b2b
Jax <jaxvanyang@gmail.com>
2021-01-26 20:28:43 +0800

    fix bowwow_and_the_timetable.cc

--- c-cpp/problem/bowwow_and_the_timetable.cc
+++ c-cpp/problem/bowwow_and_the_timetable.cc
@@ -3,6 +3,8 @@
  * @analysis 4**k 发一次，1、4、16、64
  * 1、100、10000、1000000
  * 要大于等于 4**k 必须位数要大于 2×（k-1）
+ * 1. 双位，答案就是 len / 2
+ * 2. 单位，只有一个 1 就是 len / 2；有多个就加 1
  */
 
 #include <iostream>
@@ -11,20 +13,14 @@ int main() {
   std::string s;
   std::cin >> s;
   int len = s.length();
-  if (len == 0 && s[0] == '0') {
-    std::cout << 0 << std::endl;
-    return 0;
-  }
-  int ans = (len + 1) / 2;
-  bool flag = true;
-  for (int i = 1; i < len; i++) {
-    if (s[i] == '1') {
-      flag = false;
-      break;
+  int ans = len / 2;
+  if (len % 2 == 1) {
+    for (int i = 1; i < len; i++) {
+      if (s[i] == '1') {
+        ++ans;
+        break;
+      }
     }
-  }
-  if (flag) {
-    --ans;
   }
   std::cout << ans << std::endl;
 }
