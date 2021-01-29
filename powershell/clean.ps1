a44dd2adf563ccda0e4cee76de7be008af464a16
Jax Young <jaxvanyang@gmail.com>
2021-01-29 21:47:07 +0800

    update clean.ps1

--- powershell/clean.ps1
+++ powershell/clean.ps1
@@ -1,4 +1,5 @@
 Get-ChildItem * -Include *.exe -Recurse | Remove-Item
 Get-ChildItem * -Include *.tmp -Recurse | Remove-Item
 Get-ChildItem * -Include *.txt -Recurse | Remove-Item
+Get-ChildItem * -Include *.out -Recurse | Remove-Item
 Get-ChildItem * -Include __pycache__ -Recurse | Remove-Item -Recurse
