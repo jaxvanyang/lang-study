78c590f4358c718046b641d33a978e23e1a39f4d
Jax <jaxvanyang@gmail.com>
2021-02-08 20:24:09 +0800

    update folder /java/study structure

---
+++ java/study/Main.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.*;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    StdDraw.point(1, 1);
+    StdDraw.line(0, 0, 1, 1);
+  }
+}
