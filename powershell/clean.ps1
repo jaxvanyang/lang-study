83441a8285d93f51ef13597f1476ad8d1845be62
Jax Young <jaxvanyang@gmail.com>
2021-02-08 21:09:18 +0800

    add *.class to clean.ps1

--- powershell/clean.ps1
+++ powershell/clean.ps1
@@ -1,5 +1,6 @@
-Get-ChildItem * -Include *.exe -Recurse | Remove-Item
 Get-ChildItem * -Include *.tmp -Recurse | Remove-Item
+Get-ChildItem * -Include *.exe -Recurse | Remove-Item
+Get-ChildItem * -Include *.class -Recurse | Remove-Item
 Get-ChildItem * -Include *.txt -Recurse | Remove-Item
 Get-ChildItem * -Include *.out -Recurse | Remove-Item
 Get-ChildItem * -Include __pycache__ -Recurse | Remove-Item -Recurse
