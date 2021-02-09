70912c338362ef4ca109d04702cf1ccee10af874
Jax <jaxvanyang@gmail.com>
2021-02-09 21:21:00 +0800

    add StopWatch.java 可用于获取程序运行时间

---
+++ java/study/algs4/StopWatch.java
@@ -0,0 +1,19 @@
+package algs4;
+
+public class StopWatch {
+    private final long start;
+
+    public StopWatch() {
+        start = System.currentTimeMillis();
+    }
+
+    public double elapsedTime() {
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000.0;
+    }
+
+    public static void main(String[] args) {
+        var watch = new StopWatch();
+        System.out.println(watch.elapsedTime());
+    }
+}
