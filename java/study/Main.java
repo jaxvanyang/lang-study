f55863450d7fb4b5688eafe2ef06abb879675eb9
Jax <jaxvanyang@gmail.com>
2021-02-09 17:16:05 +0800

    在 Main.java 中实现了表达式计算，但没有实现优先级的比较

--- java/study/Main.java
+++ java/study/Main.java
@@ -2,19 +2,81 @@ import edu.princeton.cs.algs4.*;
 import algs4.*;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-    StdDraw.point(1, 1);
-    StdDraw.line(0, 0, 1, 1);
+    public static void main(String[] args) {
+        String formula = "(11 + 12) * 23 - 24 / 15.9";
+        String formatedFormula = formatFormula(formula);
+        String[] parsedFormula = formatedFormula.split("\\s+");
+        for (var i : parsedFormula) {
+            StdOut.println(i);
+        }
+        MyStack<String> ops = new MyStack<>();
+        MyStack<Double> vals = new MyStack<>();
 
-    var counter = new Counter("001");
-    System.out.println(counter.tally());
-    f(counter);
-    System.out.println(counter.toString());
-    System.out.println(counter.tally());
-  }
+        for (var item : parsedFormula) {
+            if (item.length() == 0) {
+                continue;
+            }
+            if (")".equals(item)) {
+                double y = vals.pop();
+                double x = vals.pop();
+                String opt = ops.pop();
+                vals.push(calculate(x, y, opt));
+            } else if ("+-*/".indexOf(item) != -1) {
+                ops.push(item);
+            } else if ("(".equals(item)) {
+                continue;
+            } else {
+                vals.push(Double.parseDouble(item));
+            }
+        }
 
-  public static void f(Counter counter) {
-    counter.increament();
-  }
+        for (var op : ops) {
+            StdOut.print(op + " ");
+        }
+        StdOut.println();
+
+        // for (var val : vals) {
+        //     StdOut.print(val + " ");
+        // }
+
+        while (!ops.isEmpty()) {
+            StdOut.println("ops.size = " + ops.getSize() + " val.size = " + vals.getSize());
+            double y = vals.pop();
+            double x = vals.pop();
+            String opt = ops.pop();
+            vals.push(calculate(x, y, opt));
+        }
+
+        assert vals.getSize() == 1;
+        StdOut.print(vals.pop());
+    }
+
+    public static String formatFormula(String formula) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < formula.length(); ++i) {
+            char ch = formula.charAt(i);
+            if ("+-*/()".indexOf(ch) != -1) {
+                sb.append(" " + ch + " ");
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static double calculate(double x, double y, String opt) {
+        switch (opt) {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+                return x * y;
+            case "/":
+                assert y != 0;
+                return x / y;
+            default:
+                throw new RuntimeException("未定义的操作符！");
+        }
+    }
 }
