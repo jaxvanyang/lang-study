f0c6b04cf2af5375238f183327f7e49080d9341e
Jax <jaxvanyang@gmail.com>
2021-02-22 21:59:08 +0800

    another TLE and MLE solution for 2021 weekly contest 2 problem B

--- c-cpp/problem/2021_2.b.cc
+++ c-cpp/problem/2021_2.b.cc
@@ -1,23 +1,66 @@
 #include <iostream>
-// #include <map>
-#include <climits>
 #include <vector>
 
 using namespace std;
+
 const int maxn = 1e4 + 5;
 
-int graph[maxn][maxn];
+class Graph {
+ public:
+  int v, e;
+  vector<vector<vector<int>>> adj;
+  Graph(int v) : v(v) {
+    e = 0;
+    adj = vector<vector<vector<int>>>(v);
+  }
 
-int main() {
-  for (int i = 0; i < maxn; ++i) {
-    for (int j = 0; j < maxn; ++j) {
-      graph[i][j] = INT_MAX;
+  void updateEdge(int p, int q, int fee) {
+    int idx = -1;
+    for (int i = 0; i < adj[p].size(); ++i) {
+      if (adj[p][i][0] == q) {
+        idx = i;
+        break;
+      }
+    }
+    if (idx == -1) {
+      adj[p].push_back({q, fee});
+    } else {
+      adj[p][idx][1] = min(adj[p][idx][1], fee);
     }
   }
+
+  int getFee(int p, int q) {
+    for (int i = 0; i < adj[p].size(); ++i) {
+      if (adj[p][i][0] == q) {
+        return adj[p][i][1];
+      }
+    }
+    return -1;
+  }
+
+  int getAns(int s, int t) {
+    int ans = -1;
+    for (int i = 0; i < adj[s].size(); ++i) {
+      int city = adj[s][i][0];
+      int fee = adj[s][i][1];
+      for (int j = 0; j < adj[city].size(); ++j) {
+        if (adj[city][j][0] == t) {
+          if (ans == -1) {
+            ans = fee + adj[city][j][1];
+          } else {
+            ans = min(ans, fee + adj[city][j][1]);
+          }
+        }
+      }
+    }
+    return ans;
+  }
+};
+
+int main() {
   int s, t, n;
   scanf("%d%d%d", &s, &t, &n);
-  // map<int, map<int, int>> graph;  // city -> nexts = [next city -> fee]
-  // vector<vector<int>> graph(maxn, vector<int>(maxn, INT_MAX));
+  Graph graph = Graph(maxn);
 
   for (int i = 0; i < n; ++i) {
     int fee, cityCnt;
@@ -33,17 +76,17 @@ int main() {
       int p = cities[j];
       for (int k = j + 1; k < cityCnt; ++k) {
         int q = cities[k];
-        graph[p][q] = min(graph[p][q], fee);
+        graph.updateEdge(p, q, fee);
       }
     }
   }
 
-  int ans = graph[s][t];
-  // cout << ans << endl;
-  for (int i = 0; i < maxn; ++i) {
-    if (graph[s][i] != INT_MAX && graph[i][t] != INT_MAX) {
-      ans = min(ans, graph[s][i] + graph[i][t]);
-    }
+  int ans = graph.getFee(s, t);
+
+  if (ans == -1) {
+    ans = graph.getAns(s, t);
+  } else {
+    ans = min(ans, graph.getAns(s, t));
   }
 
   printf("%d\n", ans);
