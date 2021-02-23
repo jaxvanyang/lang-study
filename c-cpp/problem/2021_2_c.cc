96e8d04224b7945b985167310223a6f053d12b43
Jax <jaxvanyang@gmail.com>
2021-02-23 08:55:53 +0800

    ac solution for 2021 weekly contest 2 problem C

---
+++ c-cpp/problem/2021_2_c.cc
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class UnionFind {
+ public:
+  int cnt;
+  vector<int> ids;
+  UnionFind(int n) {
+    cnt = n;
+    for (int i = 0; i < n; ++i) {
+      ids.push_back(i);
+    }
+  }
+
+  void un(int p, int q) {
+    // ids[find(p)] = find(q);
+    int pId = find(p), qId = find(q);
+    if (pId != qId) {
+      ids[pId] = qId;
+      --cnt;
+    }
+  }
+
+  int find(int p) {
+    if (p != ids[p]) {
+      ids[p] = find(ids[p]);
+    }
+    return ids[p];
+  }
+
+  bool isConnected(int p, int q) { return find(p) == find(q); }
+};
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  vector<vector<int>> edges;
+  for (int i = 0; i < n; ++i) {
+    int cost;
+    scanf("%d", &cost);
+    edges.push_back({cost, i, n});
+    // edges.push_back({cost, n, i});
+  }
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      int cost;
+      scanf("%d", &cost);
+      // if (i != j) edges.push_back({cost, i, j});
+      if (i < j) edges.push_back({cost, i, j});
+    }
+  }
+
+  sort(edges.begin(), edges.end());
+  // 不能使用集合维护边，因为 Kruskal 算法是针对森林设计的
+  // 已经加入的点并不一定会彼此连通，它们可能处于不同的树中
+  UnionFind uf(n + 1);
+  int ans = 0;
+  int i = 0;
+  while (uf.cnt > 1) {
+    if (!uf.isConnected(edges[i][1], edges[i][2])) {
+      ans += edges[i][0];
+      uf.un(edges[i][1], edges[i][2]);
+    }
+    ++i;
+  }
+  printf("%d\n", ans);
+}
