f83aa09431f4187412288665f4b944741895b34b
Jax <jaxvanyang@gmail.com>
2021-07-17 09:37:20 +0800

    update tool: log2.cc

--- c-cpp/bin/log2.cc
+++ c-cpp/bin/log2.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 using ll = long long;
 
@@ -7,6 +8,7 @@ int h[37] = {0, 0, 1, 26, 2, 23, 27, 32, 3, 16, 24, 30, 28, 11, 33, 13, 4, 7, 17
 int parse_input(ll &n, const int argc, const char* argv[]);
 
 inline ll lowbit(ll n);
+int log2(ll n);
 
 int main(const int argc, const char* argv[]) {
 	for (int i = 0; i < 36; ++i) {
@@ -16,8 +18,9 @@ int main(const int argc, const char* argv[]) {
 	ll n = 1;
 	int ret = parse_input(n, argc, argv);
 
-	if (!ret)
-		cout << h[n % 37] << endl;
+	if (!ret) {
+		cout << log2(n) << endl;
+	}
 	
 	return ret;
 }
@@ -33,9 +36,20 @@ int parse_input(ll &n, const int argc, const char* argv[]) {
 	}
 	n = stol(argv[1]);
 
-	if (n == 0 || n - lowbit(n)) {
+	if (n == 0) {
 		cerr << "invalid argument" << endl;
 		return 2;
 	}
 	return 0;
+}
+
+int log2(ll n) {
+	while (n - lowbit(n)) {
+		n -= lowbit(n);
+	}
+	if (n > (1ll << 35)) {
+		return round(log(n) / log(2));
+	} else {
+		return h[n % 37];
+	}
 }
