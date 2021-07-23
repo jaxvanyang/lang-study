47c8aa8baffae74bc9f61e7fca24a9344842320f
Jax <jaxvanyang@gmail.com>
2021-07-23 15:52:48 +0800

    update tool: clean.sh

--- shell/clean.sh
+++ shell/clean.sh
@@ -4,6 +4,6 @@ echo `date '+%Y-%m-%d %T'` >> clean.log
 # 需要为 find 指定 -regextype 为 gnu-awk 才支持括号匹配
 # find 的正则匹配的是文件的相对路径，所以要在最前面加 .*
 find * -type d -name '__pycache__' -exec echo {}/ >> clean.log \; -exec rm -rf {} \; 
-find * -regextype gnu-awk -regex '.*\.(tmp|exe|txt|class)' -exec echo {} >> clean.log \; -delete
+find * -regextype gnu-awk -regex '.*\.(tmp|exe|out|txt|class)' -exec echo {} >> clean.log \; -delete
 echo >> clean.log
 cat clean.log && rm clean.log
