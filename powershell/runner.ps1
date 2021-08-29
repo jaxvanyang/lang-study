2cb13dcfd72bb49ed1730da84e8c586ded05cb98
Jax Young <jaxvanyang@gmail.com>
2021-08-29 19:24:25 +0800

    add script: runner.ps1

---
+++ powershell/runner.ps1
@@ -0,0 +1,37 @@
+param (
+	[string]$SourceFile
+)
+
+function Compile-SourceFile([string]$SourceFile, [string]$OutFile) {
+	g++ $SourceFile -O2 -Wall -o $OutFile
+	return $?
+}
+
+if (!(Test-Path $SourceFile)) {
+	Write-Error "Error: $SourceFile not exists"
+	return
+}
+
+$OutFile = $SourceFile -replace '.cpp', '.exe'
+if ($OutFile -like '*.cc') {
+	$OutFile = $OutFile -replace '.cc', '.exe'
+}
+
+if (!(Test-Path $OutFile)) {
+	if (!$(Compile-SourceFile $SourceFile $OutFile)) {
+		Write-Error 'Compile Error'
+		return
+	}
+} else {
+	$SourceTime = (Get-Item $SourceFile).LastWriteTime
+	$OutTime = (Get-Item $OutFile).LastWriteTime
+
+	if ($SourceTime -gt $OutTime) {
+		if (!(Compile-SourceFile $SourceFile $OutFile)) {
+			Write-Error 'Compile Error'
+			return
+		}
+	}
+}
+
+. $OutFile
