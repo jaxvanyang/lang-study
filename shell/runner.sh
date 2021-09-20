1f45f517a707b16464d188ddfc0fad0749ab546c
Jax from S3 <jaxvanyang@gmail.com>
2021-09-21 00:29:50 +0800

    update runner.sh: add stop watch & optimize

--- shell/runner.sh
+++ shell/runner.sh
@@ -26,7 +26,7 @@ run_out() {
 	if [[ "c|cpp" =~ $2 ]]; then
 		$1
 	elif [ $2 = "java" ]; then
-		java $1
+		java ${1%.*}
 	elif [ $2 = "kt" ]; then
 		java -jar $1;
 	elif [ $2 = "py" ]; then
@@ -35,22 +35,35 @@ run_out() {
 		echo "Error: Unknown file type"
 		exit 1
 	fi
+
+	if [ $? != 0 ]; then
+		echo "Error: Execution failed!"
+		exit 2
+	fi
 }
 
 # run($1=out_file, $2=lang)
 run() {
+	echo "Log: Start running"
+
+	start_time=$(date +%s%N)
+
     if [ -e data.in ]; then
         run_out $1 $2 < data.in
     else
         run_out $1 $2
     fi
+
+	end_time=$(date +%s%N)
+
+	echo "Log: Execution time: $(( (end_time - start_time) / 1000000))ms"
 }
 
 # examples are after statements
 # the default type is C++ source file
-full_name=$1;				# test.cc
-file_name=${full_name%.*};	# test
-extension=${full_name##*.};	# cc
+path=$1;					# test.cc
+file_name=${path%.*};	# test
+extension=${path##*.};	# cc
 out_file=$file_name.out;	# test.out
 
 # use normal extension as language type
@@ -58,15 +71,16 @@ lang="cpp";
 
 if [ $extension = "c" ]; then
 	lang="c"
+	out_file=$file_name.exe	# be different to C++ out file
 elif [ $extension = "java" ]; then
-	out_file=$full_name	# Java can run a single source file like python
 	lang="java"
+	out_file=$file_name.class
 elif [ $extension = "kt" ]; then
-	out_file=$file_name.jar
 	lang="kt"
+	out_file=$file_name.jar
 elif [ $extension = "py" ]; then
-	out_file=$full_name	# Python has no out file
 	lang="py"
+	out_file=$path	# Python has no out file
 elif [[ ! "cc|cpp" =~ $extension ]]; then
 	echo "Error: Unknow file type!"
 	exit 1
@@ -77,11 +91,11 @@ if [[ "c|cpp" =~ $extension ]] && [[ ! $out_file =~ "/*" ]]; then
 fi
 
 if [ -e $out_file ]; then
-    source_time=$(stat -c %Y  $full_name);
+    source_time=$(stat -c %Y  $path);
     out_time=$(stat -c %Y $out_file);
 
     if [ $source_time -lt $out_time ]; then
-		echo -e "Log: Reuse compiled file & Start running\n"
+		echo "Log: Reuse compiled file & Start running"
         run $out_file $lang
         exit 0
     fi
@@ -91,18 +105,35 @@ if [[ ! "py|java" =~ $lang ]]; then
 	echo -e "Log: Start compiling\n"
 fi
 
-log_1() {
-	echo -e "Log: Start running\n"
+# compile($1=path, $2=out_file, $3=lang)
+compile() {
+	path=$1
+	out_file=$2
+	lang=$3
+
+	if [ $lang = "py" ]; then
+		return 0
+	fi
+
+	start_time=$(date +%s%N)
+
+	if [ $lang = "cpp" ]; then
+		g++ $path -Wall -O2 -o $out_file
+	elif [ $lang = "c" ]; then
+		gcc $path -Wall -O2 -o $out_file
+	elif [ $lang = "java" ]; then
+		javac $path
+	elif [ $lang = "kt" ]; then
+		kotlinc $path -include-runtime -d $out_file
+	fi
+
+	if [ $? != 0 ]; then
+		return 3
+	fi
+
+	end_time=$(date +%s%N)
+
+	echo -e "Log: Compilation time: $(( (end_time - start_time) / 1000000 ))ms\n"
 }
 
-if [ $lang = "cpp" ]; then
-	g++ $full_name -Wall -O2 -o $out_file && log_1 && run $out_file $lang
-elif [ $lang = "c" ]; then
-	gcc $full_name -Wall -O2 -o $out_file && log_1 && run $out_file $lang
-elif [ $lang = "java" ]; then
-	log_1 && run $out_file $lang
-elif [ $lang = "kt" ]; then
-	kotlinc $full_name -include-runtime -d $out_file && log_1 && run $out_file $lang
-elif [ $lang = "py" ]; then
-	log_1 && run $out_file $lang
-fi
+compile $path $out_file $lang && run $out_file $lang
