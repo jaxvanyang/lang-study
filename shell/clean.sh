94c8c02eee9e756cd33b3e3f178288c6d3b953a5
Jax from Ubuntu <jaxvanyang@gmail.com>
2022-01-28 16:22:44 +0800

    update(clean.sh): add clean type

--- shell/clean.sh
+++ shell/clean.sh
@@ -10,4 +10,4 @@ echo 'File removed:'
 # 需要为 find 指定 -regextype 为 gnu-awk 才支持括号匹配
 # find 的正则匹配的是文件的相对路径，所以要在最前面加 .*
 find * -type d -name '__pycache__' -delete -print
-find * -regextype gnu-awk -regex '.*\.(tmp|exe|out|txt|class|in|jar)' -delete -print
+find * -regextype gnu-awk -regex '.*\.(tmp|exe|out|txt|class|in|jar|Identifier)' -delete -print
