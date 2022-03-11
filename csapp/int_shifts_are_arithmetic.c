144ece78a3a65da661831c5efd41458cd81cd89f
Jax <jaxvanyang@gmail.com>
2022-03-11 23:39:51 +0800

    add(int_shifts_are_arithmetic.c) update(.gitignore, Makefile)

---
+++ csapp/int_shifts_are_arithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int int_shifts_are_arithmetic() {
+  int shift_size = (sizeof(int) << 3) - 1;
+  int x = 0x8FFFFFFF;
+  int y = 0xFFFFFFFF;
+  return x >> shift_size == y;
+}
+
+int main() {
+  char yes_msg[] = "int shifts are arithmetic\n";
+  char no_msg[] = "int shifts are logical\n";
+  int result = int_shifts_are_arithmetic();
+  printf(result ? yes_msg : no_msg);
+
+  return result;
+}
